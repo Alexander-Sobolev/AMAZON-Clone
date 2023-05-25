20666ba701db723f59f161ed181e4dc0fc60fb9f
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-25 06:54:32 +0200

    Add Models

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma.service'
 import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService]
 })
