5130aa01ac11d77173487107d1208e9338b75149
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-25 21:31:07 +0200

    Create UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Patch,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { UserDto } from './user.dto'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: number) {
+		return this.userService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Put('profile')
+	async getNewTokens(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+		return this.userService.updateProfile(id, dto)
+	}
+
+	@HttpCode(200)
+	@Auth()
+	@Patch('profile/favorites/:productId')
+	async toggleFavorite(
+		@Param('productId') productId: string,
+		@CurrentUser('id') id: number
+	) {
+		return this.userService.toggleFavorite(id, +productId)
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException
+} from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { hash } from 'argon2'
+import { PrismaService } from 'src/prisma.service'
+import { returnUserObject } from './return-user.object'
+import { UserDto } from './user.dto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private prisma: PrismaService) {}
+
+	async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id
+			},
+			select: {
+				...returnUserObject,
+				favorites: {
+					select: {
+						id: true,
+						name: true,
+						price: true,
+						images: true,
+						slug: true
+						// category: {
+						// 	select: {
+						// 		slug: true
+						// 	}
+						// },
+						// reviews: true
+					}
+				},
+				...selectObject
+			}
+		})
+
+		if (!user) {
+			throw new Error('User not found')
+		}
+
+		return user
+	}
+
+	async updateProfile(id: number, dto: UserDto) {
+		const isSameUser = await this.prisma.user.findUnique({
+			where: { email: dto.email }
+		})
+
+		if (isSameUser && id !== isSameUser.id)
+			throw new BadRequestException('Email already in use')
+
+		const user = await this.byId(id)
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data: {
+				email: dto.email,
+				name: dto.name,
+				avatarPath: dto.avatarPath,
+				phone: dto.phone,
+				password: dto.password ? await hash(dto.password) : user.password
+			}
+		})
+	}
+
+	async toggleFavorite(userId: number, productId: number) {
+		const user = await this.byId(userId)
+
+		if (!user) throw new NotFoundException('User not found!')
+
+		const isExists = user.favorites.some(product => product.id === productId)
+
+		await this.prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				favorites: {
+					[isExists ? 'disconnect' : 'connect']: {
+						id: productId
+					}
+				}
+			}
+		})
+
+		return { message: 'Success' }
+	}
+}
